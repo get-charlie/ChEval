47e97bd8e108aa75c9142c7c8230b2bd1e179965
get-charlie <carlos.serr.dev@gmail.com>
2024-05-20 18:17:49 +0200

    Add tokenizer function

--- cheval.h
+++ cheval.h
@@ -12,7 +12,23 @@ double evaluate(const char * expression);
 
 #ifdef  IMPLEMENT_CHEVAL
 
-#define VALID_CHARS "0123456789.+-*/()"
+#define VALID_CHARS " 0123456789.+-*/()"
+#define DELIM       " "
+#define MAX_TOKS    100
+
+typedef enum{
+    NUM, ADD, SUB, MUL, DIV, OP_PAR, CL_PAR,
+}TokenType;
+
+typedef struct{
+    char* content;
+    TokenType type;
+}Token;
+
+typedef struct{
+    Token tokens[MAX_TOKS];
+    size_t size;
+}TokenList;
 
 // Test if the string contains invalid chars. true if ok false otherwise.
 static bool validate_string(const char * expression){
@@ -28,10 +44,49 @@ static bool validate_string(const char * expression){
     return ok;
 }
 
+static TokenType get_token_type(const char * content){
+   if(strcmp(content, "+") == 0){
+        return ADD;
+   }else if(strcmp(content, "-") == 0){
+        return SUB;
+   }else if(strcmp(content, "*") == 0){
+        return MUL;
+   }else if(strcmp(content, "/") == 0){
+        return DIV;
+   }else if(strcmp(content, "(") == 0){
+        return OP_PAR;
+   }else if(strcmp(content, ")") == 0){
+        return CL_PAR;
+   }else{
+        return NUM;
+   }
+}
+
+// Takes the expression and returns an array of tokens
+static TokenList tokenizer(const char * expression){
+   TokenList list; 
+   int i = 0;
+   char * t = strtok(expression, DELIM);
+   list.size = 0;
+   while(t != NULL){
+    list.tokens[i].content = t;
+    list.tokens[i].type = get_token_type(t);
+    t = strtok(NULL, DELIM);
+    list.size++;
+    i++;
+   }
+   return list;
+}
+
+#include <stdio.h>
 double evaluate(const char * expression){
     if(!validate_string(expression)){
         return NAN;
     } 
+    TokenList list = tokenizer(expression);
+    for(int i = 0; i < list.size; i++){
+        printf("Str: %s, tok: %d\n", list.tokens[i].content, list.tokens[i].type);
+    }
     return 0.0;
 }
 
